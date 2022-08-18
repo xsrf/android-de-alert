410e71605ffbe30d960370cb686d564206488eb7
xsrf <git@xsrf.de>
2022-08-18 20:35:07 +0200

    change to proper package name

--- install-apk.bat
+++ install-apk.bat
@@ -1,6 +1,4 @@
-adb shell cmd overlay disable com.example.overlaytest
 adb install -r app\build\outputs\apk\debug\app-debug.apk
-adb shell cmd overlay enable com.example.overlaytest
 adb shell su -c "stop"
 adb shell su -c "start"
 timeout 10
